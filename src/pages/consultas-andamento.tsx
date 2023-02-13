6d9234b2bd0dbefd8cc7358144d73399d91fda52
Saulo Joseph <s.de.sousa.joseph@avanade.com>
2023-02-13 17:33:05 -0300

    feat: consultas

---
+++ src/pages/consultas-andamento.tsx
@@ -0,0 +1,163 @@
+import Header from "@/components/header";
+import {
+  Accordion,
+  Button,
+  Col,
+  Container,
+  FloatingLabel,
+  Form,
+  Row,
+} from "react-bootstrap";
+
+export default function InProgressConsultation() {
+  return (
+    <>
+      <Container>
+        <Header></Header>
+        <div>
+          <Row className="mb-3">
+            <Col>
+              <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>
+                    Proprietário: Saulo Joseph | Animal: Nico
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <Row className="mb-3">
+                      <Col>
+                        <FloatingLabel label="Espécie">
+                          <Form.Control
+                            type="text"
+                            placeholder="Espécie"
+                            value="canina"
+                            disabled={true}
+                          />
+                        </FloatingLabel>
+                      </Col>
+                      <Col>
+                        <FloatingLabel label="Sexo">
+                          <Form.Select
+                            aria-label="Floating label select example"
+                            disabled={true}
+                          >
+                            <option value="3">F</option>
+                          </Form.Select>
+                        </FloatingLabel>
+                      </Col>
+                    </Row>
+                    <Row className="mb-3">
+                      <Col>
+                        <FloatingLabel label="Idade">
+                          <Form.Control
+                            type="text"
+                            disabled={true}
+                            placeholder="Idade"
+                            value="7 meses"
+                          />
+                        </FloatingLabel>
+                      </Col>
+                      <Col>
+                        <FloatingLabel label="Tamanho">
+                          <Form.Select
+                            aria-label="Floating label select example"
+                            disabled={true}
+                          >
+                            <option value="3">M</option>
+                          </Form.Select>
+                        </FloatingLabel>
+                      </Col>
+                    </Row>
+                    <Row className="mb-3">
+                      <Col>
+                        <FloatingLabel label="Pelagem">
+                          <Form.Select
+                            aria-label="Floating label select example"
+                            disabled={true}
+                          >
+                            <option value="3">Longa</option>
+                          </Form.Select>
+                        </FloatingLabel>
+                      </Col>
+                      <Col>
+                        <FloatingLabel label="Vacinações">
+                          <Form.Select
+                            aria-label="Floating label select example"
+                            disabled={true}
+                          >
+                            <option value="3">Atrasado</option>
+                          </Form.Select>
+                        </FloatingLabel>
+                      </Col>
+                    </Row>
+                    <Row className="mb-3">
+                      <Col>
+                        <FloatingLabel label="Vermífugo">
+                          <Form.Select
+                            aria-label="Floating label select example"
+                            disabled={true}
+                          >
+                            <option value="3">Atrasado</option>
+                          </Form.Select>
+                        </FloatingLabel>
+                      </Col>
+                      <Col>
+                        <FloatingLabel label="CPF">
+                          <Form.Control
+                            type="text"
+                            placeholder="CPF"
+                            value="017.458.782-89"
+                            disabled={true}
+                          />
+                        </FloatingLabel>
+                      </Col>
+                    </Row>
+                    <Row className="mb-3">
+                      <Col>
+                        <FloatingLabel
+                          controlId="floatingTextarea2"
+                          label="Anamnese"
+                        >
+                          <Form.Control
+                            as="textarea"
+                            placeholder="Leave a comment here"
+                            style={{ height: "100px" }}
+                            disabled={true}
+                            value="Machucado na pata (laceração, parcial), apetite preservado, comportamento feliz e urina normal"
+                          />
+                        </FloatingLabel>
+                      </Col>
+                    </Row>
+                    <Row className="mb-3">
+                      <Col>
+                        <div className="d-grid gap-2">
+                          <Button variant="primary" size="lg">
+                            Iniciar consulta
+                          </Button>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                  <Accordion.Header>
+                    Proprietário: Josué de Castro | Animal: Guru
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </Col>
+          </Row>
+        </div>
+      </Container>
+    </>
+  );
+}
--- src/pages/nova-consulta.tsx
+++ src/pages/nova-consulta.tsx
@@ -15,10 +15,24 @@ export default function NewConsultation() {
         <Header></Header>
         <div>
           <Row className="mb-3">
-            <Col>
-              <FloatingLabel label="Tutor">
+            <Col xs={12} sm={8} md={10} xl={11}>
+              <FloatingLabel label="Proprietário">
                 <Form.Select aria-label="Floating label select example">
-                  <option>Selecione o tutor</option>
+                  <option>Selecione o Proprietário</option>
+                  <option value="1">Cadastrar novo proprietário</option>
+                  <option value="1">Saulo Joseph</option>
+                  <option value="2">Augusto Heleno</option>
+                  <option value="3">Joseph Stalin</option>
+                </Form.Select>
+              </FloatingLabel>
+            </Col>
+          </Row>
+          <Row className="mb-3">
+            <Col xs={12} sm={8} md={10} xl={11}>
+              <FloatingLabel label="Animal">
+                <Form.Select aria-label="Floating label select example">
+                  <option>Selecione o animal</option>
+                  <option value="1">Cadastrar novo animal</option>
                   <option value="1">One</option>
                   <option value="2">Two</option>
                   <option value="3">Three</option>
@@ -28,19 +42,58 @@ export default function NewConsultation() {
           </Row>
           <Row className="mb-3">
             <Col>
-              <FloatingLabel label="Pet">
+              <FloatingLabel label="Espécie">
                 <Form.Select aria-label="Floating label select example">
-                  <option>Selecione o pet</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
+                  <option>Selecione a espécie</option>
+                  <option value="3">Canino</option>
+                  <option value="3">Felino</option>
+                </Form.Select>
+              </FloatingLabel>
+            </Col>
+            <Col>
+              <FloatingLabel label="Sexo">
+                <Form.Select aria-label="Floating label select example">
+                  <option>Selecione o sexo</option>
+                  <option value="3">F</option>
+                  <option value="3">M</option>
                 </Form.Select>
               </FloatingLabel>
             </Col>
           </Row>
           <Row className="mb-3">
             <Col>
-              <FloatingLabel label="Observações">
+              <FloatingLabel label="Pelagem">
+                <Form.Select aria-label="Floating label select example">
+                  <option>Selecione a pelagem</option>
+                  <option value="3">Longa</option>
+                  <option value="3">Curta</option>
+                </Form.Select>
+              </FloatingLabel>
+            </Col>
+            <Col>
+              <FloatingLabel label="Vacinações">
+                <Form.Select aria-label="Floating label select example">
+                  <option>Selecione a condição</option>
+                  <option value="3">Atrasadas</option>
+                  <option value="3">Em dia</option>
+                </Form.Select>
+              </FloatingLabel>
+            </Col>
+          </Row>
+          <Row className="mb-3">
+            <Col>
+              <FloatingLabel label="Vermífugo">
+                <Form.Select aria-label="Floating label select example">
+                  <option>Selecione a condição</option>
+                  <option value="3">Atrasado</option>
+                  <option value="3">Em dia</option>
+                </Form.Select>
+              </FloatingLabel>
+            </Col>
+          </Row>
+          <Row className="mb-3">
+            <Col>
+              <FloatingLabel controlId="floatingTextarea2" label="Anamnese">
                 <Form.Control
                   as="textarea"
                   placeholder="Leave a comment here"
@@ -51,9 +104,14 @@ export default function NewConsultation() {
           </Row>
           <Row className="mb-3">
             <Col>
+              <Form.Check type="checkbox" label="Retorno" />
+            </Col>
+          </Row>
+          <Row className="mb-3">
+            <Col>
               <div className="d-grid gap-2">
-                <Button variant="success" size="lg">
-                  Anexar exames
+                <Button variant="primary" size="lg">
+                  Encaminhar para consulta
                 </Button>
               </div>
             </Col>
@@ -61,8 +119,8 @@ export default function NewConsultation() {
           <Row className="mb-3">
             <Col>
               <div className="d-grid gap-2">
-                <Button variant="primary" size="lg">
-                  Salvar nova consulta
+                <Button variant="success" size="lg">
+                  Iniciar consulta
                 </Button>
               </div>
             </Col>
